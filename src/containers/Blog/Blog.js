7a6156989a30f7f16e6952bfe92d9227bcdea000
satya0306 <satyapriya0306@gmail.com>
2019-05-03 11:07:28 +0530

    implementing baseURL

--- src/containers/Blog/Blog.js
+++ src/containers/Blog/Blog.js
@@ -10,10 +10,11 @@ class Blog extends Component {
 
     state={
         posts:[],
-        selectedPostId: null 
+        selectedPostId: null,
+        error: false 
     }
     componentDidMount(){
-        axios.get('https://jsonplaceholder.typicode.com/posts')
+        axios.get('/posts')
             .then(response =>{
                 const posts = response.data.slice(0, 4);
                 const updatedPost = posts.map(post =>{
@@ -24,6 +25,9 @@ class Blog extends Component {
                 })
                 this.setState({posts:updatedPost})
                 console.log(response)
+            })
+            .catch(error => {
+                this.setState({error:true})
             });
     }
 
@@ -31,13 +35,16 @@ class Blog extends Component {
         this.setState({selectedPostId:id})
     }
     render () {
-        const posts=this.state.posts.map(post=> {
-            return <Post 
-                        key={post.id} 
-                        title={post.title} 
-                        author={post.author}
-                        clicked={() =>this.postSelectedHandler(post.id)}/>
-        })
+        let posts = <p style={{textAlign:'center'}}>Somthing wrong</p>;
+        if(!this.state.error){
+             posts=this.state.posts.map(post=> {
+                return <Post 
+                            key={post.id} 
+                            title={post.title} 
+                            author={post.author}
+                            clicked={() =>this.postSelectedHandler(post.id)}/>
+            })
+        }    
         return (
             <div>
                 <section className="Posts">
